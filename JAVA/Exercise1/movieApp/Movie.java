fc0a7dcd70c8f7a02b8c9f39b4f782d72988ce66
bhoomika0811 <bhoomika@vinsol.com>
2018-09-18 20:03:37 +0530

    Movie App Architecture Exercise

--- JAVA/Exercise1/movieApp/Movie.java
+++ JAVA/Exercise1/movieApp/Movie.java
@@ -6,61 +6,61 @@ public class Movie {
   String dish;
   String name;
   int year;
-	String language;
-	String genre;
-	String releaseDate;
-	Boolean blockbuster;
+  String language;
+  String genre;
+  String releaseDate;
+  Boolean blockbuster;
 
   static ArrayList<Movie> allMoviesList = new ArrayList<Movie>();
 
-	public Movie(String name, int year, String language, String genre, String releaseDate) {
-	  this.name = name;
-	  this.year = year;
-	  this.language = language;
-	  this.genre = genre;
-	  this.releaseDate = releaseDate;
-	  allMoviesList.add(this);
-	}
+  public Movie(String name, int year, String language, String genre, String releaseDate) {
+    this.name = name;
+    this.year = year;
+    this.language = language;
+    this.genre = genre;
+    this.releaseDate = releaseDate;
+    allMoviesList.add(this);
+  }
 
-	MovieType type;
+  MovieType type;
 
-	enum MovieType {
-	  TOLLYWOOD("Dosa"), HOLLYWOOD("Pepper Steak"), BOLLYWOOD("Dal Makhani");
+  enum MovieType {
+    TOLLYWOOD("Dosa"), HOLLYWOOD("Pepper Steak"), BOLLYWOOD("Dal Makhani");
 
-		private String dish;
-		MovieType(String dish) {
-		  this.dish = dish;
-		}
+    private String dish;
+    MovieType(String dish) {
+      this.dish = dish;
+    }
 
-		public String getDish() {
-		  return dish;
-		}
-	}
+    public String getDish() {
+      return dish;
+    }
+  }
 
-	// Getting all Movies
+  // Getting all Movies
   public static void getAllMovies() {
-	  System.out.println("List of all Movies:");
-	  for(int movieCount = 0; movieCount < allMoviesList.size(); movieCount++) {
-		  System.out.println("->" + allMoviesList.get(movieCount).name);
-	  }
-	  System.out.println();
-	}
+    System.out.println("List of all Movies:");
+    for(int movieCount = 0; movieCount < allMoviesList.size(); movieCount++) {
+      System.out.println("->" + allMoviesList.get(movieCount).name);
+    }
+    System.out.println();
+  }
 
-	// Getting Movies by type
+  // Getting Movies by type
   public static void getMoviesByType(Movie.MovieType type) {
     System.out.println("List of " + type + " Movies:" );
-		for(int movieCount = 0; movieCount < allMoviesList.size(); movieCount++)
-		if(allMoviesList.get(movieCount).type == type) {
-		  System.out.println("-> " + allMoviesList.get(movieCount).name);
-		}
-	  System.out.println();
-	}
+    for(int movieCount = 0; movieCount < allMoviesList.size(); movieCount++)
+    if(allMoviesList.get(movieCount).type == type) {
+      System.out.println("-> " + allMoviesList.get(movieCount).name);
+    }
+    System.out.println();
+  }
 
-	public boolean isBlockbuster() {
-		if(blockbuster == null) {
-			Random random = new Random();
-			this.blockbuster = random.nextBoolean();
-		}
-		return this.blockbuster;
-	}
+  public boolean isBlockbuster() {
+    if(blockbuster == null) {
+      Random random = new Random();
+      this.blockbuster = random.nextBoolean();
+    }
+    return this.blockbuster;
+  }
 }
